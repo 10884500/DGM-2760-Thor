5fd03d9a526c90d7bed1b0ab92ba182ac5656ae7
Trevor <10884500@my.uvu.edu>
2020-03-07 13:45:57 -0700

    working

--- Unit05/main.js
+++ Unit05/main.js
@@ -1,10 +1,10 @@
-const trees = ['oak', 'pine', 'Aspen', 'Bald Cypress'];
+var trees = ["oak", "pine", "Aspen", "Bald Cypress"];
 const errorElement = document.querySelector("#error");
 const displayResults = document.querySelector("#displayResults");
 
 //Display tree list inside displayResults div
 const listTrees = () => {
-  let treeList = '';
+  let treeList = "";
   trees.forEach(tree => {
     treeList += `${tree} <br>`;
   });
@@ -31,11 +31,13 @@ document.querySelector("#sortTrees").onclick = () => {
   listTrees();
 };
 
-// Lower case the trees
+// Make all the trees lower case
 document.querySelector("#lowerTrees").onclick = () => {
-    trees.toLowerCase();
-    listTrees();
-}
+  trees = trees.join(" <br>").toLowerCase();
+  trees = trees.split(" ,");
+  console.log(trees.length);
+  listTrees();
+};
 
 document.querySelector("#remove_t1").onclick = () => {
   if (trees.length > 0) {
@@ -47,6 +49,15 @@ document.querySelector("#remove_t1").onclick = () => {
   }
 };
 
+document.querySelector("#remove_t2").onclick = () => {
+  if (trees.length > 1) {
+    trees.splice(1, 1);
+    listTrees();
+  } else {
+    errorElement.textContent = "Error! Cannot remove the second tree";
+  }
+};
+
 document.querySelector("#remove_last").onclick = () => {
   if (trees.length > 0) {
     trees.pop();
@@ -54,5 +65,30 @@ document.querySelector("#remove_last").onclick = () => {
   } else {
     errorElement.textContent =
       "What's wrong with you, I can't remove a tree if there's nothing there";
+  }
+};
+
+// Name of tree #3
+document.querySelector("#showName3").onclick = () => {
+  if (trees.length > 2) {
+    let thirdTree = trees[2];
+    errorElement.textContent = thirdTree;
+    listTrees();
+  } else {
+    errorElement.innerHTML =
+      "You need a third tree in the list to get the name. <br> Add some more trees!";
+  }
+};
+
+// Name of tree #4
+document.querySelector("#showName4").onclick = () => {
+  if (trees.length > 3) {
+    let fourthTree = trees[3];
+    errorElement.textContent = fourthTree;
+    console.log(trees.length);
+    listTrees();
+  } else {
+    errorElement.innerHTML =
+      "You need a fourth tree in the list to get the name. <br> Add some more trees!";
   }
 };
