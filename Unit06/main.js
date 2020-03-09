3e64befed755f9214e9b9211d7b5195da627d36a
Trevor <10884500@my.uvu.edu>
2020-03-09 17:02:41 -0600

    messing with the story

--- Unit06/main.js
+++ Unit06/main.js
@@ -6,7 +6,7 @@ function tellStory() {
 
     var verbArray = document.querySelector('#verb').value.toLowerCase().split(/\s+/)
 
-    const myStory = `There once was a ${nounArray[2]} named Aang, who had a ${nounArray[1]} named Momo. They ${verbArray[1]} on their ${adjArray[2]} bison named Appa. Katara and Sokka were there ${verbArray[0]}ing too! <br> It took them EIGHT ${nounArray[0]} to ${verbArray[1]}, can you beleive it?! <br> the ${adjArray[1]} ${nounArray[3]} was ${verbArray[2]}ing them, and it almost ${verbArray[4]}ed ${nounArray[4]} ${adjArray[0]} bad. <br> At the ${nounArray[5]} Aang and Sokka ${verbArray[0]}ed until Mom hit them both with a(n) ${nounArray[6]}`
+    const myStory = `There once was a ${nounArray[2]} named Aang, who had a ${nounArray[1]} named Momo. They ${verbArray[1]} on their ${adjArray[2]} bison named Appa. Katara and Sokka were there ${verbArray[0]}ing too! <br> It took them eight ${nounArray[0]} to ${verbArray[1]}, can you beleive it?! <br> the ${adjArray[1]} ${nounArray[3]} was ${verbArray[2]}ing them, and it almost ${verbArray[4]}ed ${nounArray[4]} ${adjArray[0]} bad. <br> At the ${nounArray[5]} Aang and Sokka ${verbArray[0]}ed until Mom hit them both with a(n) ${nounArray[6]}`
 
     document.querySelector('#story').innerHTML=myStory
 }
