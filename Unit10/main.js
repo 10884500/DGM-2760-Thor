c5d4c25b3aecb091909e94bb964bee865fb5a19c
Trevor <10884500@my.uvu.edu>
2020-04-16 15:22:28 -0600

    fighter selection!

--- Unit10/main.js
+++ Unit10/main.js
@@ -1,10 +1,36 @@
+
+
 async function getHotelData() {
     try {
-        const response = await fetch('hotel.json')
-        return await response.json()
+        const response = await fetch("hotel.json")
+        return await response.json() 
+
     } catch (error) {
         console.error(error)
     }
 }
 
-console.log(getHotelData)
+let hotelData = {}
+
+getHotelData().then(data => hotelData = data)
+
+document.querySelector("#falco").addEventListener('click', hotelInfo)
+
+document.querySelector("#link").addEventListener('click', hotelInfo)
+
+document.querySelector("#greninja").addEventListener('click', hotelInfo)
+
+function hotelInfo(event) {
+    let hotelChoice = hotelData.hotels.find(hotel => {
+        return event.target.id === hotel.name.toLowerCase()
+    })
+    console.log(hotelChoice)
+
+    document.querySelector('#hotelName').textContent = `${hotelChoice.name}`
+    document.querySelector('#address').textContent = `${hotelChoice.address}`
+    document.querySelector('#rooms').textContent = `${hotelChoice.rooms}`
+    document.querySelector('#gym').textContent = `${hotelChoice.gym}`
+    document.querySelector('#type').textContent = `${hotelChoice.type}`
+    let picturePlacement = document.querySelector('#picture')
+    picturePlacement.src = `${hotelChoice.picture}`
+}
