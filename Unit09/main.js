2e4a7d5ae616640929091cf22859b4bb76fb122c
Trevor <10884500@my.uvu.edu>
2020-04-16 12:58:05 -0600

    finished

--- Unit09/main.js
+++ Unit09/main.js
@@ -1,16 +1,18 @@
 function duplicateMenu() {
-    let topList = document.querySelectorAll('ul#primaryNav li a')
-
+    let topList = document.querySelectorAll('ul#primaryNavigation li a')
     let newList = document.createElement('ul')
 
     topList.forEach(menuItem => {
         let newLI = document.createElement('li')
         let newLink = document.createElement('a')
         newLink.setAttribute('href', menuItem.getAttribute('href'))
+        let bottomNav = document.querySelector('#smallNavArea')
         newLink.textContent = menuItem.textContent
-        document.querySelector('#smallNavSpace').appendChild(newList)
-        newList.appendChild(newLI)
+        console.log(menuItem.textContent)
         newLI.appendChild(newLink)
+        newList.appendChild(newLI)
+        bottomNav.appendChild(newList)
     })
 }
+
 duplicateMenu()
