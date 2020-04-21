f3bac1be6a7c6d8ae399b219b8f7c178af82254b
Trevor <10884500@my.uvu.edu>
2020-04-21 12:03:43 -0600

    done, i think

--- Capstone/main.js
+++ Capstone/main.js
@@ -41,23 +41,49 @@ function fighterInfo(event) {
 
 const thumbnail = document.querySelector(".thumbnail");
 const pp = document.querySelector("#picture");
-const song = document.querySelector("#song");
+const audio = document.querySelector("#audio");
 
 var playing = true;
+var audioSrc = [
+    "/Capstone/assets/falco.mp4",
+    "/Capstone/assets/link.mp4",
+    "/Capstone/assets/greninja.mp4",
+  ],
+  audioIndex = 0;
 
 function playPause() {
   if (playing) {
-    const song = document.querySelector("#song"),
-    thumbnail = document.querySelector(".thumbnail");
-    
-    song.play();
-    playing = false;
-    thumbnail.style.transform = 'scale(1.2)';
-  } else {
-    const song = document.querySelector("#song");
-    song.pause();
-    playing = true;
-    thumbnail.style.transform = 'scale(1)';
+    const audio = document.querySelector("#audio"),
+      thumbnail = document.querySelector(".thumbnail");
 
+    audio.play();
+    playing = false;
+    thumbnail.style.transform = "scale(1.2)";
+  } else {
+    const audio = document.querySelector("#audio");
+    audio.pause();
+    playing = true;
+    thumbnail.style.transform = "scale(1)";
   }
+}
+
+function linkAudio() {
+  document.querySelector("#audio").src = audioSrc[1];
+
+    audio.pause()
+    thumbnail.style.transform = "scale(1)";
+}
+
+function falcoAudio() {
+  document.querySelector("#audio").src = audioSrc[0];
+
+  audio.pause()
+  thumbnail.style.transform = "scale(1)";
+}
+
+function grenAudio() {
+  document.querySelector("#audio").src = audioSrc[2];
+
+  audio.pause()
+  thumbnail.style.transform = "scale(1)";
 }
